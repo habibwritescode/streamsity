5183789dfd7ac4a7c645a7de34b6069081eb27e9
Habib Afolabi <habibwritescode@gmail.com>
2021-06-01 23:22:43 +0100

    Add my hosted version of cors anywhere

--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -23,7 +23,7 @@ const SearchBar = () => {
     useEffect(() => {
         if (query.trim().length < 1) return;
         dispatch(isFetching(true))
-        fetch(`https://api.deezer.com/search/?q=${query.trim()}&apiKey=${API_KEY}`, {
+        fetch(`https://habib-cors.herokuapp.com/https://api.deezer.com/search/?q=${query.trim()}&apiKey=${API_KEY}`, {
             method: 'GET'
         })
             .then(response => {
